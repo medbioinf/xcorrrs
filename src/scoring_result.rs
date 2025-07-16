5b69205ba9a5856b1f56fc9866fcc9d00ef16416
Dirk Winkelhardt <dirk.winkelhardt@rub.de>
2025-07-16 13:22:24 +0000

    implment display

--- src/scoring_result.rs
+++ src/scoring_result.rs
@@ -1,7 +1,23 @@
+use std::fmt::Display;
+
 pub struct ScoringResult {
     pub score: f64,
     pub ions_total: usize,
     pub ions_matched: usize,
     pub min_theoretical_mass: f64,
     pub max_theoretical_mass: f64,
+}
+
+impl Display for ScoringResult {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "Score: {:.2}, Ions Total: {}, Ions Matched: {}, Min Mass: {:.2}, Max Mass: {:.2}",
+            self.score,
+            self.ions_total,
+            self.ions_matched,
+            self.min_theoretical_mass,
+            self.max_theoretical_mass
+        )
+    }
 }
